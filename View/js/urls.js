f217b5bcab3093d577de87e79a7661fe7e15db13
David <daviding92@gmail.com>
2022-11-22 12:40:15 -0800

    Try setting up git ignore

--- View/js/urls.js
+++
@@ -1,3 +0,0 @@
-export const API_URL_BASE = "https://indeliblelearning.com/test/david/Controller/Api";
-export const VIEW_URL_BASE = "https://indeliblelearning.com/test/david/View";
-export const PLAYER_JOIN_URL = "https://indeliblelearning.com/electionlab";
