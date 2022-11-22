60e28a51658b1adf731a706808b517a8947a649b
David <daviding92@gmail.com>
2022-11-22 12:28:44 -0800

    Revert "Updated urls"
    This reverts commit 969bf33bbb13b5287c56721657af8dd7d77ca5a9.

--- inc/bootstrap.php
+++ inc/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
-define("CONTROLLER_API_PATH", "https://indeliblelearning.com/test/dev/Controller/Api/");
+define("CONTROLLER_API_PATH", "https://indeliblelearning.com/test/david/Controller/Api/");
  
 // include main configuration file
 require_once PROJECT_ROOT_PATH . "/inc/config.php";
--- inc/config.php
+++ inc/config.php
@@ -2,5 +2,5 @@
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "indeli9_test");
 define("DB_PASSWORD", "9]M2*h1Hpq=5");
-define("DB_DATABASE_NAME", "indeli9_election_lab_dev");
+define("DB_DATABASE_NAME", "indeli9_election_lab");
 ?>
