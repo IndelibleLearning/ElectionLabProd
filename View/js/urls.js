969bf33bbb13b5287c56721657af8dd7d77ca5a9
David <daviding92@gmail.com>
2022-11-22 12:20:18 -0800

    Updated urls

--- View/js/urls.js
+++ View/js/urls.js
@@ -1,3 +1,3 @@
-export const API_URL_BASE = "https://indeliblelearning.com/test/david/Controller/Api";
-export const VIEW_URL_BASE = "https://indeliblelearning.com/test/david/View";
-export const PLAYER_JOIN_URL = "https://indeliblelearning.com/electionlab";
+export const API_URL_BASE = "https://indeliblelearning.com/test/dev/Controller/Api";
+export const VIEW_URL_BASE = "https://indeliblelearning.com/test/dev/View";
+export const PLAYER_JOIN_URL = "https://indeliblelearning.com/electionlabdev";
